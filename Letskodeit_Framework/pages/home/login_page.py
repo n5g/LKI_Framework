a94bbbff128f64bac67a38ca83e77dfb4ef249f9
n5g <n33g@mail.ru>
2021-10-22 22:53:04 +0300

    166

--- Letskodeit_Framework/pages/home/login_page.py
+++ Letskodeit_Framework/pages/home/login_page.py
@@ -6,15 +6,38 @@ class LoginPage():
     def __init__(self, driver):
         self.driver = driver
 
-    def login(self, username, password):
-        loginLink = self.driver.find_element(By.PARTIAL_LINK_TEXT, "Login")
-        loginLink.click()
+    # locators
+    _login_link = "Login"
+    _email_field = "email"
+    _password_field = "password"
+    _login_button = "commit"
 
-        emailField = self.driver.find_element(By.ID, "email")
-        emailField.send_keys(username)
+    def getLoginLink(self):
+        return self.driver.find_element(By.PARTIAL_LINK_TEXT, self._login_link)
 
-        passwordField = self.driver.find_element(By.ID, "password")
-        passwordField.send_keys(password)
+    def getEmailField(self):
+        return self.driver.find_element(By.ID, self._email_field)
 
-        loginButton = self.driver.find_element(By.NAME, "commit")
-        loginButton.click()
+    def getPasswordField(self):
+        return self.driver.find_element(By.ID, self._password_field )
+
+    def getLoginButton(self):
+        return self.driver.find_element(By.NAME, self._login_button)
+
+    def clickLoginLink(self):
+        self.getLoginLink().click()
+
+    def enterEmail(self, email):
+        self.getEmailField().send_keys(email)
+
+    def enterPassword(self, password):
+        self.getPasswordField().send_keys(password)
+
+    def clickLoginButton(self):
+        self.getLoginButton().click()
+
+    def login(self, email, password):
+        self.clickLoginLink()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginButton()
