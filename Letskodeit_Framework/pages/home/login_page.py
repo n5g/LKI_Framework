440eddddcfaa1a7db2b179babe2aa47cb5856102
n5g <n33g@mail.ru>
2021-10-23 22:07:15 +0300

    168

--- Letskodeit_Framework/pages/home/login_page.py
+++ Letskodeit_Framework/pages/home/login_page.py
@@ -11,32 +11,34 @@ class LoginPage(SeleniumDriver): # импортировал кастомный �
     # locators
     _login_link = "Login"
     _email_field = "email"
-    _password_field = "password"
+    _password_field = "password2"
     _login_button = "commit"
 
-    def getLoginLink(self):
-        return self.driver.find_element(By.PARTIAL_LINK_TEXT, self._login_link)
-
-    def getEmailField(self):
-        return self.driver.find_element(By.ID, self._email_field)
-
-    def getPasswordField(self):
-        return self.driver.find_element(By.ID, self._password_field )
-
-    def getLoginButton(self):
-        return self.driver.find_element(By.NAME, self._login_button)
+    # def getLoginLink(self):
+    #     return self.driver.find_element(By.PARTIAL_LINK_TEXT, self._login_link)
+    #
+    # def getEmailField(self):
+    #     return self.driver.find_element(By.ID, self._email_field)
+    #
+    # def getPasswordField(self):
+    #     return self.driver.find_element(By.ID, self._password_field )
+    #
+    # def getLoginButton(self):
+    #     return self.driver.find_element(By.NAME, self._login_button)
 
     def clickLoginLink(self):
-        self.getLoginLink().click()
+        # self.getLoginLink().click()
+        self.elementClick(self._login_link, locatorType="link")
 
     def enterEmail(self, email):
-        self.getEmailField().send_keys(email)
+        # self.getEmailField().send_keys(email)
+        self.sendKeys(email, self._email_field)   # locatorType="id" locator ID по умолчанию
 
     def enterPassword(self, password):
-        self.getPasswordField().send_keys(password)
+        self.sendKeys(password, self._password_field)
 
     def clickLoginButton(self):
-        self.getLoginButton().click()
+        self.elementClick(self._login_button, locatorType="name")
 
     def login(self, email, password):
         self.clickLoginLink()
