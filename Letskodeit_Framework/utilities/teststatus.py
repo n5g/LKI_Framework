5cd6547d6bc51d4d79000506d5c555d2dc1aceba
n5g <n33g@mail.ru>
2021-10-30 18:57:23 +0300

    175

---
+++ Letskodeit_Framework/utilities/teststatus.py
@@ -0,0 +1,54 @@
+"""
+@package utilities
+
+CheckPoint class implementation
+It provides functionality to assert the result
+
+Example:
+    self.check_point.markFinal("Test Name", result, "Message")
+"""
+import lets_kode_it.Letskodeit_Framework.utilities.custom_logger as cl
+import logging
+from lets_kode_it.Letskodeit_Framework.base.selenium_driver import SeleniumDriver
+
+class TestStatus(SeleniumDriver):
+
+    log = cl.customLogger(logging.INFO)
+
+    def __init__(self, driver):
+        """
+        Inits CheckPoint class
+        """
+        super(TestStatus, self).__init__(driver)
+        self.resultList = []
+
+    def setResult(self, result, resultMessage):
+        try:
+            if result is not None:
+                if result:  # True
+                    self.resultList.append("PASS")
+                    self.log.info("### VERIFICATION SUCCESSFUL :: + " + resultMessage)
+                else:       # False
+                    self.resultList.append("FAIL")
+                    self.log.info("### VERIFICATION FAILED :: + " + resultMessage)
+            else:
+                self.resultList.append("FAIL")
+                self.log.info("### VERIFICATION FAILED :: + " + resultMessage)
+        except:
+            self.resultList.append("FAIL")
+            self.log.info("### Exception Occurred !!!")
+
+    def mark(self, result, resultMessage):
+        """
+        Mark the result of the verification point in a test case
+        """
+        self.setResult(result, resultMessage)
+
+    def markFinal(self, testName, result, resultMessage):
+        """
+        Mark the final result of the verification point in a test case
+        This needs to be called at least once in a test case
+        This should be final test status of the test case
+        """
+       
+
